f93f6d627ece1756726640f103a7c25d371bccff
mohanaTimeline <mohanapriya.narapa@outlook.com>
2024-05-25 11:29:40 -0500

    strings ctci

---
+++ workspace1/DataStructuresRevision/src/Arrays/IsomorphicStrings.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.*;
+
+public class IsomorphicStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(isomorphicStrings("egg","add"));
+	}
+
+	private static boolean isomorphicStrings(String str1, String str2) {
+		// TODO Auto-generated method stub
+		HashMap<Character,Character> hs=new HashMap<Character,Character>();
+		if(str1.length()!= str2.length())
+		{
+			return false;
+		}
+		for(int i=0;i<str1.length();i++)
+			
+		{
+			char c1= str1.charAt(i);
+			char c2= str2.charAt(i);
+			if(hs.containsKey(c1))
+			{
+				if(hs.get(c1)!=c2)
+				{
+					return false;
+				}
+			}
+			else
+			{
+				if(hs.containsValue(c2))
+				{
+					return false;
+				}
+				hs.put(c1,c2);
+			}
+		}
+		return true;
+	}
+
+}
